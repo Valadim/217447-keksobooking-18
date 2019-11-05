0e19090c0650730f247b2bc471c0b20a787d9447
Vadim <sakeyev@mail.ru>
2019-11-05 17:39:02 +0600

    Звершил задание 3.2

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ var CHECK_TIME = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var APARTMENT_TYPE = ['palace', 'flat', 'house', 'bungalo'];
 var APARTMENT_PHOTO = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-var BLOCK_SIZE = 600;
+var BLOCK_SIZE = 1100;
 var RELATIVE_ADS_COUNT = 8;
 
 var map = document.querySelector('.map');
@@ -83,12 +83,13 @@ var createAd = function (ad) {
 };
 
 var displayAds = function () {
+  var cityMap = document.querySelector('.map__pins')
   var adsList = createOffersList(RELATIVE_ADS_COUNT);
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < adsList.length; i++) {
     fragment.appendChild(createAd(adsList[i]));
   }
-  templateMapPin.appendChild(fragment);
+  cityMap.appendChild(fragment);
 };
 
 displayAds();
